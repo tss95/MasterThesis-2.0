ebfc7c92ae61bb395254bfa12e6b3ade9b4c7c55
Tord Sture Stangeland <gus008@uib.no>
2020-10-21 12:54:40 +0200

    Functioning RGS results in DF format
    Have yet to remove depreciated code

--- Classes/Modeling/RandomGridSearch.py
+++ Classes/Modeling/RandomGridSearch.py
@@ -92,6 +92,7 @@ class RandomGridSearch():
             "optimizer" : ["adam", "rmsprop", "sgd"]
         }
     model_grid = {
+        "activation" : ["relu", "sigmoid", "softmax", "tanh"],
         "start_neurons" : [2,4,8,16, 32, 64, 128, 256, 512],
         "dropout_rate" : [0.5, 0.4, 0.3, 0.2, 0.1, 0.01, 0],
         "filters" : [3, 9, 15, 21],
@@ -99,14 +100,14 @@ class RandomGridSearch():
         "padding" : ["same", "valid"],
         "l2_r" : [0.3, 0.2, 0.1, 0.01, 0.001, 0.0001],
         "l1_r" : [0.3, 0.2, 0.1, 0.01, 0.001, 0.0001],
-        "activation" : ["relu", "sigmoid", "softmax", "tanh"],
         "output_layer_activation" : ["relu", "sigmoid", "softmax", "tanh"]
     }
     
 
     def __init__(self, train_ds, val_ds, test_ds, model_nr, test, detrend, use_scaler, use_noise_augmentor,
-                 use_minmax, n_picks, hyper_grid=hyper_grid, model_grid=model_grid, num_classes = 3, 
-                 use_tensorboard = False, use_liveplots = True, use_custom_callback = False, use_early_stopping = False):
+                 use_minmax, use_highpass, n_picks, hyper_grid=hyper_grid, model_grid=model_grid, num_classes = 3, 
+                 use_tensorboard = False, use_liveplots = True, use_custom_callback = False, use_early_stopping = False,
+                 highpass_freq = 0.1):
         self.train_ds = train_ds
         self.val_ds = val_ds
         self.test_ds = test_ds
@@ -116,6 +117,7 @@ class RandomGridSearch():
         self.use_scaler = use_scaler
         self.use_noise_augmentor = use_noise_augmentor
         self.use_minmax = use_minmax
+        self.use_highpass = use_highpass
         self.n_picks = n_picks
         self.hyper_grid = hyper_grid
         self.model_grid = model_grid
@@ -124,21 +126,115 @@ class RandomGridSearch():
         self.use_liveplots = use_liveplots
         self.use_custom_callback = use_custom_callback
         self.use_early_stopping = use_early_stopping
+        self.highpass_freq = highpass_freq
         self.helper = BaselineHelperFunctions()
         self.data_gen = DataGenerator()
+    
+    def create_results_df(self):
+        hyper_keys = list(self.hyper_grid.keys())
+        model_keys = list(self.model_grid.keys())
+        metrics_train_keys = ["train_loss", "train_accuracy", "train_precision", "train_recall"]
+        metrics_val_keys = ["val_loss", "val_accuracy", "val_precision", "val_recall"]
+        header = np.concatenate((hyper_keys, model_keys, metrics_train_keys, metrics_val_keys))
+        results_df = pd.DataFrame(columns = header)
+        return results_df
+    
+    def save_results_df(self, results_df, file_name):
+        results_df.to_csv(file_name, mode = 'w')
+    
+    def clear_results_df(self, file_name):
+        path = self.get_results_file_path()
+        file = f"{path}/{file_name}"
+        if os.path.isfile(file):
+            f = open(file, "w+")
+            f.close()
         
+    
+    def get_results_file_name(self):
+        file_name = f"{self.get_results_file_path()}/results_{self.model_nr}"
+        if self.test:
+            file_name = f"{file_name}_test"
+        if self.detrend:
+            file_name = f"{file_name}_detrend"
+        if self.use_scaler:
+            if self.use_minmax:
+                file_name = f"{file_name}_mmscale"
+            else: 
+                file_name = f"{file_name}_sscale"
+        if self.use_noise_augmentor:
+            file_name = f"{file_name}_noiseAug"
+        if self.use_early_stopping:
+            file_name = f"{file_name}_earlyS"
+        if self.use_highpass:
+            file_name = f"{file_name}_highpass{self.highpass_freq}"
+        file_name = file_name + ".csv"
+        return file_name
+    
+    def get_results_file_path(self):
+        file_path = f'C:\Documents/Thesis_ssd/MasterThesis-2.0/GridSearchResults/{self.num_classes}_classes'
+        return file_path
+    
+    def store_params_before_fit(self, current_picks, results_df, file_name):
+        
+        hyper_params = current_picks[1]
+        model_params = current_picks[2]
+        print(model_params)
+        picks = []
+        for key in list(hyper_params.keys()):
+            picks.append(hyper_params[key])
+        for key in list(model_params.keys()):
+            picks.append(model_params[key])
+        nr_fillers = len(results_df.columns) - len(picks)
+        for i in range(nr_fillers):
+            picks.append(np.nan)
+        temp_df = pd.DataFrame(np.array(picks).reshape(1,21), columns = results_df.columns)
+        results_df = results_df.append(temp_df, ignore_index = True)
+        for idx, column in enumerate(results_df.columns):
+            if idx >= 13:
+                results_df[column] = results_df[column].astype('float')
+        self.save_results_df(results_df, file_name)
+        return results_df
+
+
+    def store_metrics_after_fit(self, metrics, results_df, file_name):
+        metrics_train = metrics[1]
+        metrics_val = metrics[0]
+        print(metrics_train, metrics_val)
+        finished_train = False
+        # Get list of columns containing nan values
+        unfinished_columns = results_df.columns[results_df.isnull().any()].tolist()
+        # Iterate through every unfinished column and change values
+        for idx, column in enumerate(unfinished_columns):
+            if not finished_train:
+                # Change value at column using the metrics
+                results_df.iloc[-1, results_df.columns.get_loc(column)] = metrics_train[column]
+                if idx == len(metrics_train)-1:
+                    finished_train = True
+            else:
+                # Change value at column using the metrics
+                results_df.iloc[-1, results_df.columns.get_loc(column)] = metrics_val[column]
+        self.save_results_df(results_df, file_name)
+        return results_df
+            
 
     def fit(self):
-        #num_ds, channels, timesteps = data_gen.get_trace_shape_no_cast(train_ds)
         if self.use_scaler:
             if self.use_minmax:
                 self.scaler = MinMaxScalerFitter(self.train_ds).fit_scaler(test = self.test, detrend = self.detrend)
             else:
                 self.scaler = StandardScalerFitter(self.train_ds).fit_scaler(test = self.test, detrend = self.detrend)
+        else:
+            self.scaler = None
         if self.use_noise_augmentor:
             self.augmentor = NoiseAugmentor(self.train_ds, self.use_scaler, self.scaler)
-        self.delete_progress()
-        self.save_conditionals()
+        else:
+            self.augmentor = None
+        
+        # Create name of results file, clear contents if one exists, create df to work with.
+        self.results_file_name = self.get_results_file_name()
+        self.clear_results_df(self.results_file_name)
+        self.results_df = self.create_results_df()
+        
         self.hyper_picks = self.get_n_params_from_list(list(ParameterGrid(self.hyper_grid)), self.n_picks)
         self.model_picks = self.get_n_params_from_list(list(ParameterGrid(self.model_grid)), self.n_picks)
         self.results = []
@@ -146,7 +242,10 @@ class RandomGridSearch():
         for i in range(self.n_picks):
             model_info = {"model_nr" : self.model_nr, "index" : i}
             current_picks = [model_info, self.hyper_picks[i], self.model_picks[i]]
-            self.save_params(current_picks)
+            # Store picked parameters:
+            self.results_df = self.store_params_before_fit(current_picks, self.results_df, self.results_file_name)
+            
+            # Translate picks to a more readable format:
             epoch = self.hyper_picks[i]["epochs"]
             batch_size = self.hyper_picks[i]["batch_size"]
             dropout_rate = self.model_picks[i]["dropout_rate"]
@@ -160,18 +259,25 @@ class RandomGridSearch():
             padding = self.model_picks[i]["padding"]
             opt = self.getOptimizer(self.hyper_picks[i]["optimizer"], self.hyper_picks[i]["learning_rate"])
             
+            # Generate build model args using the picks from above.
             build_model_args = self.helper.generate_build_model_args(self.model_nr, batch_size, dropout_rate, 
                                                                      activation, output_layer_activation,
                                                                      l2_r, l1_r, start_neurons, filters, kernel_size, 
                                                                      padding, self.num_classes)
+            # Build model using args generated above
             model = Models(**build_model_args).model
+            
+            # Generate generator args using picks.
             gen_args = self.helper.generate_gen_args(batch_size, self.test, self.detrend, use_scaler = self.use_scaler, scaler = self.scaler, use_noise_augmentor = self.use_noise_augmentor, augmentor = self.augmentor, num_classes = self.num_classes)
             
+            # Initiate generators using the args
             train_gen = self.data_gen.data_generator(self.train_ds, **gen_args)
             val_gen = self.data_gen.data_generator(self.val_ds, **gen_args)
             test_gen = self.data_gen.data_generator(self.test_ds, **gen_args)
             
+            # Generate compiler args using picks
             model_compile_args = self.helper.generate_model_compile_args(opt, self.num_classes)
+            # Compile model using generated args
             model.compile(**model_compile_args)
             
             print("Starting: ")
@@ -180,24 +286,29 @@ class RandomGridSearch():
             pp.pprint(self.model_picks[i])
 
             
-
+            # Generate fit args using picks.
             fit_args = self.helper.generate_fit_args(self.train_ds, self.val_ds, batch_size, self.test, 
                                                      epoch, val_gen, use_tensorboard = self.use_tensorboard, 
                                                      use_liveplots = self.use_liveplots, 
                                                      use_custom_callback = self.use_custom_callback,
                                                      use_early_stopping = self.use_early_stopping)
+            # Fit the model using the generated args
             model_fit = model.fit(train_gen, **fit_args)
             
-            loss, accuracy, precision, recall = model.evaluate_generator(generator=test_gen,
-                                                                         steps=self.helper.get_steps_per_epoch(self.test_ds, 
-                                                                                                               batch_size, False))
+            # Evaluate the fitted model on the validation set
+            loss, accuracy, precision, recall = model.evaluate_generator(generator=val_gen,
+                                                                       steps=self.helper.get_steps_per_epoch(self.val_ds, 
+                                                                                                             batch_size, False))
+            # Record metrics for train
             metrics = []
-            metrics_test = {"val_loss" : loss,
+            metrics_val = {"val_loss" : loss,
                             "val_accuracy" : accuracy,
                             "val_precision": precision,
                             "val_recall" : recall}
-            metrics.append(metrics_test)
-            current_picks.append(metrics_test)
+            metrics.append(metrics_val)
+            current_picks.append(metrics_val)
+            
+            # Evaluate the fitted model on the train set
             train_loss, train_accuracy, train_precision, train_recall = model.evaluate_generator(generator=train_gen,
                                                                                         steps=self.helper.get_steps_per_epoch(self.train_ds,
                                                                                                                               batch_size,
@@ -208,7 +319,7 @@ class RandomGridSearch():
                              "train_recall" : train_recall}
             metrics.append(metrics_train)
             current_picks.append(metrics_train)
-            self.save_metrics(metrics)
+            self.results_df = self.store_metrics_after_fit(metrics, self.results_df, self.results_file_name)
             self.results.append(current_picks)
             
         highest_test_accuracy_index, highest_train_accuracy_index, highest_test_precision_index, highest_test_recall_index = self.find_best_performers(self.results)
@@ -228,32 +339,32 @@ class RandomGridSearch():
         highest_train_recall = 0
         highest_train_recall_index = 0
         for idx, result in enumerate(results):
-            if result[3]["test_accuracy"] > highest_test_accuracy:
-                highest_test_accuracy = result[3]["test_accuracy"]
+            if result[3]["val_accuracy"] > highest_test_accuracy:
+                highest_test_accuracy = result[3]["val_accuracy"]
                 highest_test_accuracy_index = idx
             if result[4]["train_accuracy"] > highest_train_accuracy:
                 highest_train_accuracy = result[4]["train_accuracy"]
                 highest_train_accuracy_index = idx
-            if result[3]["test_precision"] > highest_test_precision:
-                highest_test_precision = result[3]["test_precision"]
+            if result[3]["val_precision"] > highest_test_precision:
+                highest_test_precision = result[3]["val_precision"]
                 highest_test_precision_index = idx
             if result[4]["train_precision"] > highest_train_precision:
                 highest_train_precision = result[4]["train_precision"]
                 highest_train_precision_index = idx
-            if result[3]["test_recall"] > highest_test_recall:
-                highest_test_recall = result[3]["test_recall"]
+            if result[3]["val_recall"] > highest_test_recall:
+                highest_test_recall = result[3]["val_recall"]
                 highest_test_recall_index = idx
             if result[4]["train_recall"] > highest_train_recall:
                 highest_train_recall = result[4]["train_recall"]
                 highest_train_recall_index = idx
         print("----------------------------------------------------ACCURACY------------------------------------------------------")
-        print(f'Highest test accuracy: {highest_test_accuracy}, at index: {highest_test_accuracy_index}')
+        print(f'Highest val accuracy: {highest_test_accuracy}, at index: {highest_test_accuracy_index}')
         print(f'Highest training accuracy: {highest_train_accuracy}, at index: {highest_train_accuracy_index}')
         print("----------------------------------------------------PRECISION-----------------------------------------------------")
-        print(f'Highest test precision: {highest_test_precision}, at index: {highest_test_precision_index}')
+        print(f'Highest val precision: {highest_test_precision}, at index: {highest_test_precision_index}')
         print(f'Highest training precision: {highest_train_precision}, at index: {highest_train_precision_index}') 
         print("-----------------------------------------------------RECALL-------------------------------------------------------")
-        print(f'Highest test recall: {highest_test_recall}, at index: {highest_test_recall_index}')
+        print(f'Highest val recall: {highest_test_recall}, at index: {highest_test_recall_index}')
         print(f'Highest training recall: {highest_train_recall}, at index: {highest_train_recall_index}')
         print("------------------------------------------------------------------------------------------------------------------")
         return highest_test_accuracy_index, highest_train_accuracy_index, highest_test_precision_index, highest_test_recall_index
@@ -264,11 +375,13 @@ class RandomGridSearch():
         dictionaries = []
         with open(text_file, 'r') as file:
             for idx, line in enumerate(file):
-                if idx == 0:
+                if idx == 0 or idx == 1:
                     continue
                 line = re.sub("\'", "\"", line.rstrip())
-                if idx % 6 != 0: 
+                if idx + 2 % 5 != 0: 
+                    print(line)
                     dictionaries.append(json.loads(line))
+            file.close()
         dictionaries_by_model = []
         one_model = []
         for idx, dictionary in enumerate(dictionaries):
